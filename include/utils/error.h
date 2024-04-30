91e916824e6bca8834774382c97d2f3d2eca664a
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-05-01 00:27:31 +0100

    add include/utils/error.h

---
+++ include/utils/error.h
@@ -0,0 +1,34 @@
+#ifndef ERROR_H
+#define ERROR_H
+
+/***************************************************************************
+ * Project          ____ ____    _    ____  __  __      ____   ____
+ *                 / ___/ ___|  / \  / ___||  \/  |    | ___| / ___|
+ *                 \___ \___ \ / _ \ \___ \| |\/| |____|___ \| |  _
+ *                  ___) |__) / ___ \ ___) | |  | |_____|__) | |_| |
+ *                 |____/____/_/   \_\____/|_|  |_|    |____/ \____|
+ *
+ *
+ * Author: David Carvalheiro, 2022220112
+ * Author: Luís Góis,         2018280716
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the LICENSE file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/
+
+#define HANDLE_ERROR(MSG)                                       \
+	do {                                                    \
+		fprintf(stderr, "%s:%d: ", __FILE__, __LINE__); \
+		perror(MSG);                                    \
+		exit(EXIT_FAILURE);                             \
+	} while (0);
+
+#endif // !ERROR_H
