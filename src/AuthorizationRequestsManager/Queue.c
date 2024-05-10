ce8bf814656d523724e0184b487e71e10abc43c9
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-05-10 11:37:06 +0100

    added Queue.c

---
+++ src/AuthorizationRequestsManager/Queue.c
@@ -0,0 +1,43 @@
+/***************************************************************************
+ * Project          ____ ____    _    ____  __  __      ____   ____
+ *                 / ___/ ___|  / \  / ___||  \/  |    | ___| / ___|
+ *                 \___ \___ \ / _ \ \___ \| |\/| |____|___ \| |  _
+ *                  ___) |__) / ___ \ ___) | |  | |_____|__) | |_| |
+ *                 |____/____/_/   \_\____/|_|  |_|    |____/ \____|
+ *
+ *
+ * Author: David Carvalheiro, 2022220112
+ * Author: Luís Góis,         2018280716
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the LICENSE file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/
+
+#include "AuthorizationRequestsManager/Queue.h"
+
+#include "AuthorizationRequest.h"
+
+#include <stddef.h>
+#include <stdlib.h>
+
+Queue createQueue(const size_t size)
+{
+	static const Queue invalidQueue = {0};
+
+	Queue queue = {
+	    .size     = size,
+	    .writePos = 0,
+	    .readPos  = 0,
+	    .data     = calloc(size, sizeof(AuthorizationRequest)),
+	};
+
+	return queue.data == NULL ? invalidQueue : queue;
+}
