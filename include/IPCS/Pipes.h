f990e7cccfef188591c24943580566b2a84a583e
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-05-13 14:28:45 +0100

    add license header to pipes files

--- include/IPCS/Pipes.h
+++ include/IPCS/Pipes.h
@@ -1,6 +1,29 @@
 #ifndef SSASM_5G_IPCS_PIPES_H
 #define SSASM_5G_IPCS_PIPES_H
 
+/***************************************************************************
+ * Project          ____ ____    _    ____  __  __      ____   ____
+ *                 / ___/ ___|  / \  / ___||  \/  |    | ___| / ___|
+ *                 \___ \___ \ / _ \ \___ \| |\/| |____|___ \| |  _
+ *                  ___) |__) / ___ \ ___) | |  | |_____|__) | |_| |
+ *                 |____/____/_/   \_\____/|_|  |_|    |____/ \____|
+ *
+ *
+ * Author: David Carvalheiro, 2022220112
+ * Author: Luís Góis,         2018280716
+ *
+ * This software is licensed as described in the file LICENSE, which
+ * you should have received as part of this distribution.
+ *
+ * You may opt to use, copy, modify, merge, publish, distribute and/or sell
+ * copies of the Software, and permit persons to whom the Software is
+ * furnished to do so, under the terms of the LICENSE file.
+ *
+ * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
+ * KIND, either express or implied.
+ *
+ ***************************************************************************/
+
 #define USER_PIPE             "/tmp/user-pipe.fifo"
 #define USER_PIPE_PERMISSIONS 0644
 #define BACK_PIPE             "/tmp/back-pipe.fifo"
