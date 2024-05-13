6c56ed874eac3b3131a175d0a879d0df869e7020
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-05-14 00:57:53 +0100

    pass config authServcersMax to authorizationrequestsmanager

--- src/SystemManager.c
+++ src/SystemManager.c
@@ -89,7 +89,8 @@ int main(int argc, char **argv)
 	sharedStatsID  = createSharedMemory(sizeof(Statistics), false);
 	messageQueueID = createMessageQueue();
 
-	FORK_FUNCTION(authorizationRequestsManager);
+	FORK_FUNCTION(authorizationRequestsManager,
+	              systemManagerConfig.options.authServersMax);
 	FORK_FUNCTION(monitorEngine);
 
 	int status;
